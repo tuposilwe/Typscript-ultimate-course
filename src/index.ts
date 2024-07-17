b25e5b6e758ad7e87690c3412682b044bf052564
tuposilwe <rudigermkondya@gmail.com>
2024-07-17 12:38:43 +0300

    type aliases

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-let employee: {
+type Employee = {
     readonly id: number,
     name: string,
     retire: (date: Date) => void
-} ={
+}
+
+
+let employee: Employee = {
     id: 1,
     name: 'Mosh',
     retire: (date: Date) => {
