feb6aa0b0047506e40898253d1d78bbce3d1d831
tuposilwe <rudigermkondya@gmail.com>
2024-07-17 13:16:18 +0300

    type assertions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
-let speed: number | null = null;
-
-let ride = {
-    //Falsy (undefined, null, '', false, 0)
-    //Nullish coalescing operator
-    speed: speed ?? 30
-}
+let phone = document.getElementById('phone') as HTMLInputElement;
+// HTMLElement
+// HTMLInputElement
+phone.value
