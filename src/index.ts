a582be6d1b3cdb7f54777d2a6df071513cce3bff
tuposilwe <rudigermkondya@gmail.com>
2024-07-17 12:12:55 +0300

    Objects

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-function calculateTax(income: number,taxYear?: number): number{
-    if ((taxYear || 2022) < 2022)
-        return income * 1.2;
-    return income * 1.3;
+let employee: {
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+} ={
+    id: 1,
+    name: 'Mosh',
+    retire: (date: Date) => {
+        console.log(date)
+    }
 }
-
-calculateTax(10_000);
