82e3fb0ffa940ccb2b09ecf98f51a7e0db687b7c
tuposilwe <rudigermkondya@gmail.com>
2024-07-17 12:53:51 +0300

    literal types

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-type Draggable = {
-    drag: () => void
-};
-
-type Resizable = {
-    resize: () => void
-};
-
-type UIWidget = Draggable & Resizable;
-
-
-let textBox: UIWidget = {
-    drag: () => {},
-    resize: () => {}
-}
+// Literal (exact, specific)
+type Quantity  = 50 | 100;
+let quantity: Quantity = 100;
