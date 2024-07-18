1f848a28a2daa1535456a8d04896024171593782
tuposilwe <rudigermkondya@gmail.com>
2024-07-18 11:20:19 +0300

    abstract classes and methods

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,15 @@
-class Person{
-    constructor(
-        public firstName: string,
-        public lastName: string
-    ){}
+abstract class Shape {
+    constructor(public color: string) {}
 
-    get fullName(){
-        return this.firstName + ' ' + this.lastName;
+    abstract render():void;
+}
+
+class Circle extends Shape{
+    constructor(public radius: number,color: string){
+        super(color);
     }
 
-    walk(){
-    console.log('Walking');
-   }
-}
-
-
-class Student extends Person {
- constructor(public studentId: number,firstName: string,lastName: string){
-    super(firstName,lastName);
- }
-
- takeTest(){
-    console.log('Taking a test');
- }
-
-}
-
-class Teacher extends Person{
-   override get fullName(){
-        return 'Professor ' + super.fullName;
+    override render(): void {
+        console.log('Rendering a circle');
     }
-}
-
-printNames([
-    new Teacher('John','Smith'),
-    new Student(1,'Rudiger','Deskillz47')
-])
-
-function printNames(people: Person[]){
-    for (let person of people)
-        console.log(person.fullName)
 }
