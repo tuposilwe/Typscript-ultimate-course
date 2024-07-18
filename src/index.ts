484913b9c0d04b2b80b224c8ac50f7aaf84fead0
tuposilwe <rudigermkondya@gmail.com>
2024-07-18 09:40:53 +0300

    Index signatures

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,11 @@
-class Account {
-    nickname?: string;
-
-    constructor(
-        public readonly id: number,
-        public owner: string,
-        private _balance: number){
-    }
-
-    deposit(amount: number): void{
-        if(amount <= 0)
-            throw new Error('Invalid amount');
-        this._balance += amount;
-    }
-
-    get balance(): number {
-       return this._balance;
-    }
-
-    set balance(value: number){
-      if(value < 0)
-        throw new Error('Invalid value');
-     this._balance = value;
-    }
+class SeatAssignment{
+    //A1, A2, ...
+    // Mosh , John, ...
+    //Index signature property
+    [seatNumber: string]: string;
 }
 
-let account = new Account(1, 'Mosh',0);
-console.log(account.balance);
+let seats = new SeatAssignment();
+seats.A1 = 'Mosh';
+seats['A1'] = 'Mosh';
+seats.A2 = 'John';
