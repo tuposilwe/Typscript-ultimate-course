9b79863bb6ea231bfcad336b161432d2a018cb25
tuposilwe <rudigermkondya@gmail.com>
2024-07-18 09:13:15 +0300

    Creating Objects

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,17 @@
 "use strict";
-let phone = document.getElementById('phone');
-phone.value;
+class Account {
+    constructor(id, owner, balance) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+    deposit(amount) {
+        if (amount <= 0)
+            throw new Error('Invalid amount');
+        this.balance += amount;
+    }
+}
+let account = new Account(1, 'Mosh', 0);
+account.deposit(100);
+console.log(account instanceof Account);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,7 @@ class Account {
         this.balance += amount;
     }
 }
+
+let account = new Account(1, 'Mosh',0);
+account.deposit(100);
+console.log(account instanceof Account);
