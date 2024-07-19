254e3510f30113c7923e06afcbbf4505875ffb07
tuposilwe <rudigermkondya@gmail.com>
2024-07-19 09:31:59 +0300

    The Key of Operator

--- src/index.ts
+++ src/index.ts
@@ -9,23 +9,15 @@ class Store<T>{
     add(obj: T): void {
        this._objects.push(obj);
     }
+
+// T is Product
+//Keyof T => 'name' | 'price'
+   find(property: keyof T, value: unknown): T | undefined {
+    return this._objects.find(obj => obj[property] === value);
+   }
 }
 
-// Pass on the generic type parameter
-class CompressibleStore<T> extends Store<T>{
-    compress() {}
-}
-
-// Restrict the generic type parameter
-class SearchableStore<T extends { name: string }> extends Store<T> {
-    find(name: string): T | undefined{
-        return this._objects.find(obj => obj.name === name);
-    }
-}
-
-// Fix the generic type parameter
-class ProductStore extends Store<Product>{
-    filterByCategory(category: string): Product[]{
-           return [];
-    }
-}
+let store = new Store<Product>();
+store.add({name: 'a',price: 1});
+store.find('name','a');
+store.find("price",'ss')
