e5b28aff59cb5bf4be0018968baa1a3552476d90
tuposilwe <rudigermkondya@gmail.com>
2024-07-19 11:37:30 +0300

    Parameterized Decorators

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,19 @@
-function MinLength(length: number) {
-    return (target: any, propertyName: string) => {
-        let value: string;
+type WatchedParameter = {
+    methodName: string, 
+    parameterIndex: number
+};
 
-        const descriptor: PropertyDescriptor = {
-            get() {return value;},
-            set(newValue: string){
-                if (newValue.length < length)
-                    throw new Error(`${propertyName} should be at least ${length} characters long.`)
-                value = newValue;
-            }
-        };
-        Object.defineProperty(target, propertyName, descriptor);
-    }
+const watchedParameters:WatchedParameter[] =[]
+
+function Watch(target: any, methodName: string, parameterIndex: number){
+       watchedParameters.push({
+        methodName,
+        parameterIndex
+       });
 }
 
-
-class User {
-    @MinLength(4)
-    password: string;
-
-    constructor(password: string) {
-        this.password = password;
-    }
+class Vehicle {
+    move(@Watch speed:number) {}
 }
 
-let user = new User('1ddd');
-user.password='wddw'
-console.log(user.password);
+console.log(watchedParameters);
