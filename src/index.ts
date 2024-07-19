06e1058fcd7a7e3f4d2f7c57ddb9bcd903282795
tuposilwe <rudigermkondya@gmail.com>
2024-07-19 11:28:47 +0300

    Property Decorators

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,29 @@
+function MinLength(length: number) {
+    return (target: any, propertyName: string) => {
+        let value: string;
 
-function Capitalize(target: any, methodName: string, descriptor: PropertyDescriptor){
-    const original = descriptor.get;
-    descriptor.get = function() {
-        const result = original?.call(this);
-        return (typeof result === 'string') ? result.toUpperCase() : result;
+        const descriptor: PropertyDescriptor = {
+            get() {return value;},
+            set(newValue: string){
+                if (newValue.length < length)
+                    throw new Error(`${propertyName} should be at least ${length} characters long.`)
+                value = newValue;
+            }
+        };
+        Object.defineProperty(target, propertyName, descriptor);
     }
 }
 
-class Person{
-    constructor(public firstName: string, public lastName: string){}
 
-     @Capitalize
-     get fullName(){
-        return `${this.firstName} ${this.lastName}`;
+class User {
+    @MinLength(4)
+    password: string;
+
+    constructor(password: string) {
+        this.password = password;
     }
 }
 
-
-let person = new Person('Rudiger','deskizll');
-console.log(person.fullName);
+let user = new User('1ddd');
+user.password='wddw'
+console.log(user.password);
