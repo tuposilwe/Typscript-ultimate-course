e6b4016ff8952037c8e7f9512c9f292c2fe23c5b
tuposilwe <rudigermkondya@gmail.com>
2024-07-20 08:57:43 +0300

    Describing Types using JSDoc

--- src/tax.js
+++ src/tax.js
@@ -1,3 +1,10 @@
+/**
+ * Calculates income tax.
+ * @param {number} income - Net salary after expenses. 
+ * @returns {number}
+ */
+
+
 export function calculateTax(income){
     return income * .3;
 }
